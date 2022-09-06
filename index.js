26b1a0480a8b3d8d153f3031fc4b927fb3ac67dc
martinsarmiento <sarmientomartin678@gmail.com>
2022-09-05 23:12:15 -0300

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 const cosasQueAprendimos = [
   {
-    tema: "terminal",
-    class: "",
+    tema: 'terminal',
+    class: '',
   },
   {
-    tema: "node",
-    class: "",
+    tema: 'node',
+    class: '',
   },
   {
-    tema: "oop",
-    class: "",
+    tema: 'oop',
+    class: '',
   },
   {
-    tema: "typescript",
-    class: "",
+    tema: 'typescript',
+    class: '',
   },
   {
-    tema: "css",
-    class: "",
+    tema: 'css',
+    class: '',
   },
   {
-    tema: "dom",
-    class: "special",
+    tema: 'dom',
+    class: 'special',
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector('.lista');
+  const listaLiEl = document.querySelectorAll('.lista li');
+
+  for (const iterator of listaLiEl) {
+    iterator.remove();
+  }
+
+  for (const item of cosasQueAprendimos) {
+    const newLiEl = document.createElement('li');
+    newLiEl.textContent = item.tema;
+    const iteratorCosas = item.class || 'item';
+    newLiEl.classList.add(iteratorCosas);
+
+    listaEl.appendChild(newLiEl);
+  }
+}
 
 main();
